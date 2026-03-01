32531886c583160fca1826da0eda9e9c777acee2
Styles <kartik100100@gmail.com>
2026-03-01 23:08:05 +0530

    build fix

--- apps/web/src/components/landing/our-vision.tsx
+++
@@ -1,8 +0,0 @@
-import { StaggerItem, StaggerText } from "@/components/animations/stagger-text";
-import { Section } from "@/components/layout/section";
-
-export function OurVision() {
-	return (
-		<Section className="glass bg-secondary pb-0 text-secondary-foreground lg:pb-0"></Section>
-	);
-}
